7108c62b298a4ea507ddbd26f60cb0718333a19c
Enamul <muhammad.enamul.haque79@gmail.com>
2023-03-28 19:56:08 +0600

    cart finished

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,17 +3,23 @@ import './Cart.css';
 
 const Cart = ({cart}) => {
     console.log(cart);
+
     let total = 0;
+    let totalShipping = 0;
+
     for(const product of cart){
         total = total + product.price;
+        totalShipping = totalShipping + product.shipping;
     }
+
+    const tax = total * 5/100;
    return (
         <div className ='cart'>
             <h2>Order Summery</h2>
             <h6>Selected Items : {cart.length}</h6>
-            <h6>Total Price : {total}</h6>
-            <h6>Total Shipping Charge   :</h6>
-            <h6>Tax :</h6>
+            <h6>Total Price : ${total}</h6>
+            <h6>Shipping Charge : ${totalShipping}</h6>
+            <h6>Tax : ${tax.toFixed(2)}</h6>
             <h4>Grand Total :</h4>
         </div>
     );
